5a61cde92c8ae814e9500051e2f22f187cf346e3
Rémi LEBEGUE <remilebegue@hotmail.fr>
2015-09-07 17:34:38 +0200

    Commit du soir BONSOIR

--- Form113/Areas/Admin/Controllers/StatistiqueController.cs
+++ Form113/Areas/Admin/Controllers/StatistiqueController.cs
@@ -9,14 +9,18 @@ namespace Form113.Areas.Admin.Controllers
 {
     public class StatistiqueController : Controller
     {
-        public JsonResult GetJSONStatistique()
+        public JsonResult GetJSONStatistique(string id)
         {
             BestArtEntities  db = new BestArtEntities();
 
-            var V = db.Visibiliter.Where(pt => pt.DateVis >= new DateTime(2015, 1, 1) && pt.DateVis < new DateTime(2015, 12, 31))
-                      .Join(db.Produits, pt => pt.ProduitRef, p => p.IdProduit, (pt, p) => new { pt.DateVis, p.Nom })
-                      .GroupBy(x => x.Nom)
-                      .Select(g => new { Nom = g.Key.Trim(), NbVis = g.Count() })
+            var val = id.Split('-');
+            int min = int.Parse(val[0]);
+            int max = int.Parse(val[1]);
+
+            var V = db.Visibiliter.Where(pt => pt.DateVis >= new DateTime(min, 1, 1) && pt.DateVis < new DateTime(max, 12, 31))
+                      .Join(db.Produits, pt => pt.ProduitRef, p => p.IdProduit, (pt, p) => new { pt.DateVis, p.Pays })
+                      .GroupBy(x => x.Pays)
+                      .Select(g => new { Nom = g.Key.Name.Trim(), NbVis = g.Count() })
                       .OrderBy(x => x.Nom);
             return Json(V, JsonRequestBehavior.AllowGet);
         }
@@ -24,6 +28,12 @@ namespace Form113.Areas.Admin.Controllers
         // GET: Admin/Statistique
         public ActionResult Index()
         {
+            
+            BestArtEntities  db = new BestArtEntities();
+            ViewBag.min = db.Visibiliter.Min(x => x.DateVis);
+            ViewBag.max = db.Visibiliter.Max(x => x.DateVis);
+
+
             return View();
         }
     }
