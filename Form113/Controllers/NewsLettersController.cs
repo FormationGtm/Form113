45810abf7f9f75cbc96ab5d0687b9c1beeffcef8
Junqua <greg.junqua@gmail.com>
2015-09-08 09:58:26 +0200

    Partie User Newsletter

--- Form113/Controllers/NewsLettersController.cs
+++ Form113/Controllers/NewsLettersController.cs
@@ -15,34 +15,64 @@ namespace Form113.Controllers
         // GET: NewsLetter
         public ActionResult Index()
         {
-            
+
             var newsletter = new Form113.Models.NewsLettersViewModel();
             newsletter.Message = "Pas d'action possible";
             return View(newsletter);
         }
-       
 
         public ActionResult Delete(string id)
         {
-            var newsletter = new Form113.Models.NewsLettersViewModel();
-            var identities = db.Identites.Where(x => x.Email == id);
-            if (identities.Count() == 1)
+            var newsletter = new Form113.Models.NewsLettersViewModel() { Id = int.Parse(id) };
+            return View(newsletter);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Form113.Models.NewsLettersViewModel newsletter)
+        {
+            var identities = db.Identites.Find(newsletter.Id);
+            if (identities == null)
             {
-                identities.First().Newsletter = 0;
-                db.Entry(identities).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-               newsletter.Message="Changement réussi";
+                newsletter.Message = "Votre adresse n'est pas reconnue";
             }
-            else if (identities.Count() > 1)
+            else
             {
-                newsletter.Message="Email non trouve contacter l'administrateur";
+                if (newsletter.Email == identities.Email)
+                {
+                    newsletter.Message = "Votre desinscription à la newsletter vient d etre effectué.";
+                    identities.Newsletter = 0;
+                    db.Entry(identities).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    newsletter.Message = "Adresse Email introuvable.";
+                }
             }
-            else { newsletter.Message="L'email specifie ne correspond a aucun email dans notre base de donnée"; }
-            
-            
-            
             return View("Index", newsletter);
         }
+
+        //public ActionResult Delete(string id)
+        //{
+        //    var newsletter = new Form113.Models.NewsLettersViewModel();
+        //    var identities = db.Identites.Where(x => x.Email == id);
+        //    if (identities.Count() == 1)
+        //    {
+        //        identities.First().Newsletter = 0;
+        //        db.Entry(identities).State = System.Data.Entity.EntityState.Modified;
+        //        db.SaveChanges();
+        //       newsletter.Message="Changement réussi";
+        //    }
+        //    else if (identities.Count() > 1)
+        //    {
+        //        newsletter.Message="Email non trouve contacter l'administrateur";
+        //    }
+        //    else { newsletter.Message="L'email specifie ne correspond a aucun email dans notre base de donnée"; }
+
+
+
+        //    return View("Index", newsletter);
+        //}
     }
 
 }
--- Form113/Models/NewsLettersViewModel.cs
+++ Form113/Models/NewsLettersViewModel.cs
@@ -8,5 +8,7 @@ namespace Form113.Models
     public class NewsLettersViewModel
     {
        public string Message { get; set; }
+       public string Email { get; set; }
+       public int Id { get; set; }
     }
 }
