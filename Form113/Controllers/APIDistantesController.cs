c767d8484ba2a5adc4a1823d670a9e958f0221c9
Yoann THERY <yoann.thery@gmail.com>
2015-09-08 18:16:29 +0200

    commit final

---
+++ Form113/Controllers/APIDistantesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form113.Controllers
+{
+    public class APIDistantesController : Controller
+    {
+        // GET: APIDistantes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult partenaire()
+        {
+            return PartialView("_partenaire");
+        } 
+    }
+}
---
+++ Form113/Scripts/Partenaires/partenaire114.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    var uri = 'http://form114.dlucazeau.fr/api/produitsapi';
+    // Send an AJAX request
+    $.getJSON(uri){console.log(data);}
+        //.done(function (data) {
+            
+            // On success, 'data' contains a list of products.
+            // $.each(data, function (key, item) {
+            // Add a list item for the product.
+            // $('<li>', { text: key+" : "+item }).appendTo($('#content'));
+            //  });
+        });
