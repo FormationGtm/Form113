a11a6342b40bc364a69c7c636bd122bc30d81fb1
Junqua <greg.junqua@gmail.com>
2015-09-08 18:16:08 +0200

    machin

--- Form113TestUnit/UnitTest1.cs
+++ Form113TestUnit/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using DataLayer;
+using DataLayer.Models;
 namespace Form113TestUnit
 {
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// This test check if the giveMeList Method return a list with the type of command
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
+            var ListToTest = Commande.giveMeList();
+           
+            foreach (var item in ListToTest)
+            {
+                var commandeFirst = new Commande(item);
+                break;
+            }
+
+            var typeToCheck = ListToTest.GetType();
+            Assert.AreEqual(commandeFirst.Nom,"NomTest");
         }
     }
 }
